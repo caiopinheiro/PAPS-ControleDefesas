489a6c711ad7cf5b0f96ce07968bfb3714fc5d55
Caio Pinheiro <pinheiro.caiof@gmail.com>
2014-11-19 16:47:53 -0400

    Criação dos dois modulos que faltavam

---
+++ com_defesascoordenador/controller.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @version     1.0.0
+ * @package     com_defesascoordenador
+ * @copyright   Copyright (C) 2014. Todos os direitos reservados.
+ * @license     GNU General Public License versão 2 ou posterior; consulte o arquivo License. txt
+ * @author      Caio <pinheiro.caiof@gmail.com> - http://
+ */
+// No direct access
+defined('_JEXEC') or die;
+
+jimport('joomla.application.component.controller');
+
+class DefesascoordenadorController extends JController {
+
+    /**
+     * Method to display a view.
+     *
+     * @param	boolean			$cachable	If true, the view output will be cached
+     * @param	array			$urlparams	An array of safe url parameters and their variable types, for valid values see {@link JFilterInput::clean()}.
+     *
+     * @return	JController		This object to support chaining.
+     * @since	1.5
+     */
+    public function display($cachable = false, $urlparams = false) {
+        require_once JPATH_COMPONENT . '/helpers/defesascoordenador.php';
+
+        parent::display($cachable, $urlparams);
+
+        return $this;
+    }
+
+}
