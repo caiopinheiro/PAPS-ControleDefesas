20cf7393b9df5c634c345ad3e6bb4773af151867
thiagoleitexd <thiagoleitexd@gmail.com>
2014-11-21 10:35:09 -0400

    Update default.php
    modificações no link do icones de funcionalidades, bem como modificações nas task para haver concistencia com o arquivo controller.php

--- com_controledefesas/views/controledefesas/tmpl/default.php
+++ com_controledefesas/views/controledefesas/tmpl/default.php
@@ -15,59 +15,41 @@ defined('_JEXEC') or die('Restricted access');
 <div class="cpanel">
 	
 		<link rel="stylesheet" type="text/css" href="components/com_controledefesas/assets/css/template.css">
+	
+	
 		
 		<div class="icon-wrapper">
 			<div class="icon">
-				<a href="index.php?option=com_controledefesas&view=controledefesas&task=solicitarBanca">
-				<img width="32" height="32" border="0" src="components/com_controledefesas/assets/images/professor.png"><span><?php echo JText::_( 'Solicitar Banca' ); ?></span></a>
-			</div>
-		</div>
-		
-		<div class="icon-wrapper">
-			<div class="icon">
-				<a href="index.php?option=com_portalsecretaria&Itemid=<?php echo $Itemid;?>&task=funcionarios">
-				<img width="32" height="32" border="0" src="components/com_controledefesas/assets/images/aprovarcandidatos.png"><span><?php echo JText::_( 'Aprovar Banca' ); ?></span></a>
-			</div>
-		</div>
-		
-		<div class="icon-wrapper">
-			<div class="icon">
-				<a href="index.php?option=com_portalsecretaria&Itemid=<?php echo $Itemid;?>&task=funcionarios">
+				<a href="index.php?option=com_controledefesas&Itemid=<?php echo $Itemid;?>&task=confirmarbanca">
 				<img width="32" height="32" border="0" src="components/com_controledefesas/assets/images/aprovarcandidatos.png"><span><?php echo JText::_( 'Confirmar Banca' ); ?></span></a>
 			</div>
 		</div>
+	
 		
 		<div class="icon-wrapper">
 			<div class="icon">
-				<a href="index.php?option=com_portalsecretaria&Itemid=<?php echo $Itemid;?>&task=funcionarios">
-				<img width="32" height="32" border="0" src="components/com_controledefesas/assets/images/historico.gif"><span><?php echo JText::_( 'Lançar Conceito' ); ?></span></a>
-			</div>
-		</div>
-		
-		<div class="icon-wrapper">
-			<div class="icon">
-				<a href="index.php?option=com_portalsecretaria&Itemid=<?php echo $Itemid;?>&task=funcionarios">
+				<a href="index.php?option=com_controledefesas&Itemid=<?php echo $Itemid;?>&task=consultardefesas">
 				<img width="32" height="32" border="0" src="components/com_controledefesas/assets/images/consultar.gif"><span><?php echo JText::_( 'Consultar Defesas' ); ?></span></a>
 			</div>
 		</div>
 	
 		<div class="icon-wrapper">
 			<div class="icon">
-				<a href="index.php?option=com_portalsecretaria&Itemid=<?php echo $Itemid;?>&task=funcionarios">
+				<a href="index.php?option=com_controledefesas&Itemid=<?php echo $Itemid;?>&task=gerenciarmembros">
 				<img width="32" height="32" border="0" src="components/com_controledefesas/assets/images/alunos.png"><span><?php echo JText::_( 'Gerenciar Membros da Banca' ); ?></span></a>
 			</div>
 		</div>
 		
 		<div class="icon-wrapper">
 			<div class="icon">
-				<a href="index.php?option=com_portalsecretaria&Itemid=<?php echo $Itemid;?>&task=funcionarios">
+				<a href="index.php?option=com_controledefesas&Itemid=<?php echo $Itemid;?>&task=enviaremail">
 				<img width="32" height="32" border="0" src="components/com_controledefesas/assets/images/enviar.gif"><span><?php echo JText::_( 'Enviar Emails de aviso' ); ?></span></a>
 			</div>
 		</div>
 		
 		<div class="icon-wrapper">
 			<div class="icon">
-				<a href="index.php?option=com_portalsecretaria&Itemid=<?php echo $Itemid;?>&task=avaliarcandidatos">
+				<a href="index.php?option=com_controledefesas&Itemid=<?php echo $Itemid;?>&task=gerarrelatorios">
 				<img width="32" height="32" border="0" src="components/com_controledefesas/assets/images/relatorio.png"><span><?php echo JText::_( 'Gerar Relatórios' ); ?></span></a>
 			</div>
 		</div>
