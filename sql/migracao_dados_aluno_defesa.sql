c1c00e482c7c7d8a395f0ddec3644cb2a37b9a62
caiopinheiro <pinheiro.caiof@gmail.com>
2014-12-12 12:59:09 -0400

    atualização sql de migração

--- sql/migracao_dados_aluno_defesa.sql
+++ sql/migracao_dados_aluno_defesa.sql
@@ -3,7 +3,7 @@
 
 -- antes de executar script no banco de dados, colocar j17_defesa.idDefesa com autoincrement
 insert into j17_defesa (aluno_id, conceito, tipoDefesa, data, titulo, local, horario, resumo)
-select matricula, conceitoQual2, 'Q1',  str_to_date(dataQual2, '%d/%m/%Y'), tituloQual2,
+select id, conceitoQual2, 'Q1',  str_to_date(dataQual2, '%d/%m/%Y'), tituloQual2,
 localQual2, horarioQual2, resumoQual2
 from j17_aluno 
 where 
@@ -13,7 +13,7 @@ and
 and curso = 1;
 
 insert into j17_defesa (aluno_id, data, titulo, tipoDefesa, conceito, resumo, local, horario, numDefesa)
-select matricula,  str_to_date(dataTese, '%d/%m/%Y'), tituloTese, 'D', conceitoTese, resumoTese, localTese, horarioTese, numDefesa
+select id,  str_to_date(dataTese, '%d/%m/%Y'), tituloTese, 'D', conceitoTese, resumoTese, localTese, horarioTese, numDefesa
 from j17_aluno 
 where 
 (str_to_date(dataTese, '%d/%m/%Y')  <> '00-00-0000')
@@ -22,7 +22,7 @@ and
 and curso = 1; 
 
 insert into j17_defesa (aluno_id, data, titulo, tipoDefesa, conceito)
-select matricula, str_to_date(dataQual1, '%d/%m/%Y'), tituloQual1, 'Q1', conceitoQual1 
+select id, str_to_date(dataQual1, '%d/%m/%Y'), tituloQual1, 'Q1', conceitoQual1 
 from j17_aluno 
 where 
 (str_to_date(dataQual1, '%d/%m/%Y') <> '00-00-0000')
@@ -31,7 +31,7 @@ and
 and curso = 2;
 
 insert into j17_defesa (aluno_id, data, titulo, tipoDefesa, conceito, resumo, local, horario)
-select matricula,  str_to_date(dataQual2, '%d/%m/%Y') dataq2, tituloQual2, 'Q1', conceitoQual2, resumoQual2, localQual2, horarioQual2
+select id,  str_to_date(dataQual2, '%d/%m/%Y') dataq2, tituloQual2, 'Q1', conceitoQual2, resumoQual2, localQual2, horarioQual2
 from j17_aluno 
 where 
 (str_to_date(dataQual2, '%d/%m/%Y')  <> '00-00-0000')
@@ -40,10 +40,10 @@ and
 and curso = 2;
 
 insert into j17_defesa (aluno_id, data, titulo, tipoDefesa, conceito, resumo, local, horario, numDefesa)
-select matricula,  str_to_date(dataTese, '%d/%m/%Y'), tituloTese, 'T', conceitoTese, resumoTese, localTese, horarioTese, numDefesa
+select id,  str_to_date(dataTese, '%d/%m/%Y'), tituloTese, 'T', conceitoTese, resumoTese, localTese, horarioTese, numDefesa
 from j17_aluno 
 where 
 (str_to_date(dataTese, '%d/%m/%Y')  <> '00-00-0000')
 and 
 ((tituloTese <> '') and (tituloTese is not null) and (length(tituloTese) > 0))  
-and curso = 2;
+and curso	 = 2;
