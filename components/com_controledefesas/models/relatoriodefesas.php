0a7cfeabc03c340c69b59be50c7643e3da851b48
kalleycorrea <kalleycorrea@gmail.com>
2015-02-23 13:55:50 -0400

    Correção bug - Relatório de Defesas por período

--- components/com_controledefesas/models/relatoriodefesas.php
+++ components/com_controledefesas/models/relatoriodefesas.php
@@ -27,13 +27,12 @@ class ControledefesasModelRelatorioDefesas extends JModelItem
                 }
                 return $this->msg;
         }
-
-
 	    public function getDefesasPorPeriodo($data_inicial, $data_final) {
 			$database =& JFactory::getDBO();
 			$sql_standard = "SELECT d.*, date_format(d.data,'%d/%m/%Y') as data_defesa, ifnull(d.conceito, '') as conceito_defesa,
 							       a.nome as nome_aluno, a.id as idAluno, a.curso, 
 							       (case a.curso when 1 then 'Mestrado' when 2 then 'Doutorado' when 3 then 'Especial' end) as desc_curso, 
+							       d.tipoDefesa, 
 							       (case d.tipoDefesa 
 							     		when upper('Q1') then 'Qualificação 1' 
 							     		when upper('Q2') then 'Qualificação 2' 
@@ -46,26 +45,23 @@ class ControledefesasModelRelatorioDefesas extends JModelItem
 			
 			$sql_data_inicial = '';
 			$sql_data_final = '';
-
 			$sql_orderby = " ORDER BY date_format(d.data,'%d/%m/%Y')";
 			
 			if ($data_inicial != '')
 				$sql_data_inicial = " AND d.data >= STR_TO_DATE('".$data_inicial."','%d-%m-%Y')";
-
 			if ($data_final != '')
 				$sql_data_final = " AND d.data <= STR_TO_DATE('".$data_final."','%d-%m-%Y')";
-
 			$sql = $sql_standard.$sql_data_inicial.$sql_data_final.$sql_orderby;
 			
 			$database->setQuery($sql);
 			return $database->loadObjectList();
 		}
-
 	    public function getDefesasPorPeriodoProfessor($data_inicial, $data_final, $nome_professor) {
 			$database =& JFactory::getDBO();
 			$sql_standard = "SELECT d.*, date_format(d.data,'%d/%m/%Y') as data_defesa, ifnull(d.conceito, '') as conceito_defesa,
-							       a.nome as nome_aluno, a.id as idAluno, a.curso, 
+							       a.nome as nome_aluno, a.id as idAluno, a.curso, p.nomeProfessor as nome_orientador, d.examinador, 
 							       (case a.curso when 1 then 'Mestrado' when 2 then 'Doutorado' when 3 then 'Especial' end) as desc_curso, 
+							       d.tipoDefesa, 
 							       (case d.tipoDefesa 
 							     		when upper('Q1') then 'Qualificação 1' 
 							     		when upper('Q2') then 'Qualificação 2' 
@@ -76,28 +72,29 @@ class ControledefesasModelRelatorioDefesas extends JModelItem
     									when 'P' then 'Presidente' 
     									when 'I' then 'Membro Interno' 
     									when 'E' then 'Membro Externo' end) as funcao_membro 
-								FROM ((((
+								FROM (((((
 								      j17_defesa as d 
 								      	LEFT JOIN j17_aluno as a ON a.id = d.aluno_id) 
-							        	LEFT JOIN j17_banca_controledefesas as bcd ON d.banca_id = bcd.id) 
+							        	LEFT JOIN j17_banca_controledefesas as bcd ON d.banca_id = bcd.id AND ifnull(bcd.status_banca, '') = '1') 
 							        	LEFT JOIN j17_banca_has_membrosbanca as bhm ON bcd.id = bhm.banca_id)
 							        	LEFT JOIN j17_membrosbanca as mb ON bhm.membrosbanca_id = mb.id)
-								WHERE ifnull(d.conceito, '') <> '' ";
+										LEFT JOIN j17_professores as p ON p.id = a.orientador)
+								WHERE d.idDefesa is not null ";
 			
 			$sql_data_inicial = '';
 			$sql_data_final = '';
 			$sql_nome_professor = '';
-
 			$sql_orderby = " ORDER BY date_format(d.data,'%d/%m/%Y')";
-
+			
 			if ($data_inicial != NULL && $data_inicial != false && $data_inicial != '')
 				$sql_data_inicial = " AND d.data >= STR_TO_DATE('".$data_inicial."','%d-%m-%Y')";
-
+			
 			if ($data_final != NULL && $data_final != false && $data_final != '')
 				$sql_data_final = " AND d.data <= STR_TO_DATE('".$data_final."','%d-%m-%Y')";
-
-			if($nome_professor != '')
-				$sql_nome_professor = " AND upper(mb.nome) LIKE upper('%".$nome_professor.'%\')';
+			
+			if($nome_professor != ''){
+				$sql_nome_professor = " AND (upper(mb.nome) LIKE upper('%".$nome_professor."%') OR upper(p.nomeProfessor) LIKE upper('%".$nome_professor."%') OR upper(d.examinador) LIKE upper('%".$nome_professor."%'))";
+			}
 
 			$sql = $sql_standard.$sql_data_inicial.$sql_data_final.$sql_nome_professor.$sql_orderby;
 			$database->setQuery($sql);
