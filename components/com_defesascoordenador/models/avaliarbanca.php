5ce731b164fa5d19b9b0298d5cfefd46beaa2715
caiopinheiro <pinheiro.caiof@gmail.com>
2015-02-25 18:57:30 -0400

    alteração do sql avaliarBanca

--- components/com_defesascoordenador/models/avaliarbanca.php
+++ components/com_defesascoordenador/models/avaliarbanca.php
@@ -35,14 +35,14 @@ class DefesasCoordenadorModelAvaliarBanca extends JModelItem
 		public function visualizarMembrosBanca($idBanca){
 			$database =& JFactory::getDBO();
 
-			$sql1 = "(select concat('Prof. ', p.nomeProfessor) nome, 'P' funcao, 'PPGI/UFAM' filiacao, p.email, 'N' passagem
+		/*	$sql1 = "(select concat('Prof. ', p.nomeProfessor) nome, 'P' funcao, 'PPGI/UFAM' filiacao, p.email, 'N' passagem
 			from ((j17_professores p join j17_aluno a on a.orientador = p.id) join j17_defesa d on d.aluno_id = a.id) join j17_banca_controledefesas b on b.id = d.banca_id
 			where b.id = $idBanca )";
-			
+		*/	
 			
 			$sql = "(SELECT mb.nome, bhmb.funcao, mb.filiacao, mb.email, bhmb.passagem FROM  #__banca_has_membrosbanca AS bhmb JOIN #__membrosbanca AS mb ON mb.id = bhmb.membrosbanca_id WHERE bhmb.banca_id = ".$idBanca . ')';
 			
-			$sql = $sql1 . 'UNION ' . $sql;
+		//	$sql = $sql1 . 'UNION ' . $sql;
 			
 			
 	//		var_dump(%sql)
